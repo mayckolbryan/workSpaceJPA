bd591ab4118a90af8d994542c01bb252d91836d6
mayckolbryan <mayckolbryan226@gmail.com>
2018-11-22 18:29:39 -0500

    creacion de entidad, modificaciones en pom.xml y utilizando junit para creacion de esquemas en oracle

--- jpaProject/src/test/java/com/arquitecturajava/test/NoticiaTest.java
+++ jpaProject/src/test/java/com/arquitecturajava/test/NoticiaTest.java
@@ -3,10 +3,14 @@
  */
 package com.arquitecturajava.test;
 
+import javax.persistence.Persistence;
+
 /**
  * @author User
  *
  */
 public class NoticiaTest {
-
+	public void iniciar(){
+		Persistence.generateSchema("curso", null);
+	}
 }
