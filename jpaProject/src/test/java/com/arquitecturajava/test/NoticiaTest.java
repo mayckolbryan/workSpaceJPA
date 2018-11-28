61050018518d48212f2122f24a8eeec5d7b63a20
mayckolbryan <mayckolbryan226@gmail.com>
2018-11-28 18:40:57 -0500

    corrigiendo error persistence.xml hibernate - persistence provider

--- jpaProject/src/test/java/com/arquitecturajava/test/NoticiaTest.java
+++ jpaProject/src/test/java/com/arquitecturajava/test/NoticiaTest.java
@@ -3,14 +3,31 @@
  */
 package com.arquitecturajava.test;
 
+import static org.junit.Assert.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * @author User
  *
  */
 public class NoticiaTest {
+	EntityManagerFactory emf;
+	EntityManager em;
+	
+	@Before
 	public void iniciar(){
 		Persistence.generateSchema("curso", null);
+		emf = Persistence.createEntityManagerFactory("curso");
+	}
+	
+	@Test
+	public void entityManagerFactoryOK(){
+		assertNotNull(emf);
 	}
 }
